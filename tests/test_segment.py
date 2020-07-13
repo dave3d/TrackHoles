69d2b614e3c0f77e9dd67028ba756d5a11190a09
Dave Chen <dchen@mail.nih.gov>
2020-07-13 11:24:53 -0400

    flake8 updates

--- tests/test_segment.py
+++ tests/test_segment.py
@@ -1,19 +1,21 @@
 #! /usr/bin/env python
 
-import SimpleITK as sitk
 import unittest
 
+import SimpleITK as sitk
 import segment
+
 
 class TestSegment(unittest.TestCase):
 
     def test_segmentBackground(self):
-        print ("\nTesting segmentBackground")
-        img = sitk.GaussianSource(sitk.sitkUInt16, [100,100], [20,25], [50,50], 60000)
+        print("\nTesting segmentBackground")
+        img = sitk.GaussianSource(sitk.sitkUInt16, [100, 100], [20, 25],
+                                  [50, 50], 60000)
 
         img2 = segment.segmentBackground(img)
-        components = sitk.ConnectedComponent(img2,img2)
-        #sitk.Show(components)
+        components = sitk.ConnectedComponent(img2, img2)
+        # sitk.Show(components)
 
         stats = sitk.LabelShapeStatisticsImageFilter()
         stats.Execute(components)
@@ -24,13 +26,13 @@ class TestSegment(unittest.TestCase):
         axes = stats.GetPrincipalAxes(1)
         moments = stats.GetPrincipalMoments(1)
         npix = stats.GetNumberOfPixels(1)
-        print ("Center:", c)
-        print ("Axes:", axes)
-        print ("Moments:", moments)
-        print ("# pixels:", npix)
+        print("Center:", c)
+        print("Axes:", axes)
+        print("Moments:", moments)
+        print("# pixels:", npix)
 
-        self.assertEqual(c, (50.0,50.0))
-        self.assertEqual(axes, (1.0,0.0,0.0,1.0))
+        self.assertEqual(c, (50.0, 50.0))
+        self.assertEqual(axes, (1.0, 0.0, 0.0, 1.0))
         self.assertAlmostEqual(moments[0], 371.119403)
         self.assertAlmostEqual(moments[1], 587.553116769)
         self.assertEqual(npix, 2278)
--- tests/test_track_hole.py
+++ tests/test_track_hole.py
@@ -1,16 +1,18 @@
 #! /usr/bin/env python
 
+import math
+import os
 import unittest
-import math, os
-import SimpleITK as sitk
 
-import track_hole
+import SimpleITK as sitk
 import draw_ellipses
+import track_hole
+
 
 class TestTrackHole(unittest.TestCase):
 
     def test_track_hole(self):
-        print ("\nTesting track_hole")
+        print("\nTesting track_hole")
 
         slices = []
         seed = []
@@ -18,46 +20,46 @@ class TestTrackHole(unittest.TestCase):
 
         for z in range(0, 400):
             zf = float(z)
-            xsize = 40.+10.0*math.cos(zf/8.0)
-            ysize = 50.-10.0*math.sin(zf/10.0)
-            xpos = 200.0+zf/5.0
-            ypos = 300.0-zf/6.0
+            xsize = 40. + 10.0 * math.cos(zf / 8.0)
+            ysize = 50. - 10.0 * math.sin(zf / 10.0)
+            xpos = 200.0 + zf / 5.0
+            ypos = 300.0 - zf / 6.0
 
-            zslice = sitk.BinaryThreshold( sitk.GaussianSource( sitk.sitkUInt8, [512,512],
-                sigma=[xsize, ysize], mean=[xpos, ypos]), 200, 255, 0, 1 )
+            zslice = sitk.BinaryThreshold(sitk.GaussianSource(
+                sitk.sitkUInt8, [512, 512], sigma=[xsize, ysize],
+                mean=[xpos, ypos]), 200, 255, 0, 1)
 
-            centers.append([xpos,ypos])
+            centers.append([xpos, ypos])
 
-            if len(seed)==0:
-               seed=[xpos-5, ypos+6]
-            slices.append( zslice )
+            if len(seed) == 0:
+                seed = [xpos - 5, ypos + 6]
+            slices.append(zslice)
 
         radius = 100
         ellipses = track_hole.track_hole(seed, radius, slices)
-        #print(ellipses)
+        # print(ellipses)
         img = sitk.JoinSeries(slices)
         result = draw_ellipses.draw_ellipses(img, ellipses)
 
         if "SITK_SHOW" in os.environ:
-          sitk.Show(result)
+            sitk.Show(result)
 
-        count=0
-        fail_count=0
-        for e, c in zip(ellipses,centers):
+        count = 0
+        fail_count = 0
+        for e, c in zip(ellipses, centers):
             ce = e[0]
-            dx = c[0]-ce[0]
-            dy = c[1]-ce[1]
-            d = math.sqrt(dx*dx+dy*dy)
-            if d>.5:
-               print("\nBad center", count, round(d,3))
-               print(c)
-               print(e)
-               fail_count = fail_count+1
-            count = count+1
+            dx = c[0] - ce[0]
+            dy = c[1] - ce[1]
+            d = math.sqrt(dx * dx + dy * dy)
+            if d > .5:
+                print("\nBad center", count, round(d, 3))
+                print(c)
+                print(e)
+                fail_count = fail_count + 1
+            count = count + 1
         if fail_count:
             print("%d center errors total", fail_count)
             self.fail()
-
 
 
 if __name__ == "__main__":
